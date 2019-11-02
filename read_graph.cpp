656c775bcda0cce70a9b545fbc4a502a5d1f7193
Ionwyn Sean <ionwyn@Ionwyns-MacBook-Air.local>
2019-11-01 18:22:25 -0700

    Finish Reading Graph

--- read_graph.cpp
+++ read_graph.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <utility>
 
 using namespace std;
 
-/* The undirected weighted graph G is a set of vertices with the properties:
-	- Each vertex has a set of neighbours
-	- A path between two adjacent vertices has a traversal cost
-	- Each vertex is defined by coordinate (latitude, longitude)
+int main() {
+	// Placeholder
+	string str = "";
 
-*/
+	ifstream input;
 
+	input.open("graph1000.txt");
+	if (!input)
+	{
+	    cerr << "Failed opening graph1000.txt" << endl;
+	    exit(1);
+	}
+
+	int i = 0;
+	while(i < 1000)
+	{
+
+		cout << "Index " << i + 1 << endl;
+		// ignore(<number of characters to ignore until delimiter seen>, <delimiting character>)
+	    input.ignore(5, ':');
+	    input.ignore(1, ' ');
+
+	    // The third argument ',' is delimiter.  When used, getline stops reading right there
+	    getline(input, str, ',');
+	    cout << "X: " << str << endl;
+	    input.ignore(1);
+
+	    getline(input, str);
+	    cout << "Y: " << str << endl;
+	    cout << endl;
+	    
+	    i++;
+	}
+
+	return 0;
+}
