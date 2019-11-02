d61f05506f3f7ddb276b29ec243edfca0781e472
Ionwyn Sean <ionwyn@Ionwyns-MacBook-Air.local>
2019-11-01 17:36:02 -0700

    Start Read graph1000.txt

---
+++ read_graph.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <utility>
+
+using namespace std;
+
+/* The undirected weighted graph G is a set of vertices with the properties:
+	- Each vertex has a set of neighbours
+	- A path between two adjacent vertices has a traversal cost
+	- Each vertex is defined by coordinate (latitude, longitude)
+
+*/
+
